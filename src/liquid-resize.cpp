c2a4db60e1d48652dd3aa766e62c031900d2b569
Mitchell Henry <mitchell.henry334@gmail.com>
2022-02-12 12:31:16 +1100

    added .gitignore and cleaned up image reading code

--- src/liquid-resize.cpp
+++ src/liquid-resize.cpp
@@ -1,19 +1,19 @@
 #include "liquid-resize.hpp"
 #include <OpenImageIO/imageio.h>
 #include <iostream>
+#include <stdexcept>
 
 using namespace OIIO;
 
 LiquidResize::LiquidResize(std::string const& path) {
     auto inp = ImageInput::open(path);
-    if (! inp)
-        return;
-    const ImageSpec &spec = inp->spec();
-    int xres = spec.width;
-    int yres = spec.height;
-    int channels = spec.nchannels;
-    std::cout << "w = " << xres << " h = " << yres << " channels = " << channels << "\n";
-    std::vector<unsigned char> pixels(xres * yres * channels);
+    if (not inp) {
+        auto err = "Failed to open image: " + path + "\n";
+        throw std::runtime_error(err);
+    }
+    auto const& spec = inp->spec();
+    std::cout << "w = " << spec.width << " h = " << spec.height << " channels = " << spec.nchannels << "\n";
+    auto pixels = std::vector<unsigned char>(spec.width * spec.height * spec.nchannels);
     inp->read_image(TypeDesc::UINT8, &pixels[0]);
     inp->close();
 }
